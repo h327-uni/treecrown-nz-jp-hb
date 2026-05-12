7e342a89bea52905f8d4710376eb2bf911ab8d66
Joel Plummer <jplu652@aucklanduni.ac.nz>
2026-05-12 14:06:29 +1200

    Scaffold package

--- treecrown/src/treecrown_nz_jp_hb/__init__.py
+++ treecrown/src/treecrown_nz_jp_hb/__init__.py
@@ -1,5 +1,21 @@
-def hello() -> str:
-    return "Hello from treecrown-nz-jp-hb!"
+"""Your one-line package description."""
+
+from __future__ import annotations
+# import pandas as pd
+
+__version__ = "0.1.0"
 
 
-print(hello)
+def load_canopy(bbox):
+    """Fetches the Auckland Council canopy layer from LINZ for a given bounding box and returns a GeoDataFrame of canopy polygons."""
+    raise NotImplementedError
+
+
+def canopy_coverage(area_gdf):
+    """Computes the percentage of each input polygon covered by canopy."""
+    raise NotImplementedError
+
+
+def route_shade(route_gdf, buffer_m):
+    """Buffers each edge of a route by a given distance (in metres) and returns the canopy fraction within that buffer, a proxy for pedestrian shade."""
+    raise NotImplementedError
