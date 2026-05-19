e2ad0830316f41ddf5ba861bfe61f84aa6fd9723
jarm704-boop <jarm704@aucklanduni.ac.nz>
2026-05-19 13:14:10 +1200

    Update __init__.py

--- treecrown/src/treecrown_nz_jp_hb/__init__.py
+++ treecrown/src/treecrown_nz_jp_hb/__init__.py
@@ -19,3 +19,5 @@ def canopy_coverage(area_gdf):
 def route_shade(route_gdf, buffer_m):
     """Buffers each edge of a route by a given distance (in metres) and returns the canopy fraction within that buffer, a proxy for pedestrian shade."""
     raise NotImplementedError
+
+test
